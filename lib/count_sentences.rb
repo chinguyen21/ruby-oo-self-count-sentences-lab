9adcd0e65e8fd578fca1920ad21fc089541f260f
chinguyen21 <nguyen.thimai@northeastern.edu>
2020-11-17 20:49:49 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    first_split = self.split(".").map {|sentence| sentence.split("?")}.flatten
+    second_split = first_split.map {|sentence| sentence.split("!")}
+    final_split = second_split.flatten
+    final_split.select {|string| string.length != 0}.length
   end
 end
